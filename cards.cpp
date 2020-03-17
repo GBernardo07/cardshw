0f11ca0e9a430386f4755416a861c3406f690247
Gabriel <gbernardoreis@outlook.com>
2020-03-17 12:23:27 -0300

    implemented read_cards() //  print_cards() not working properly

--- cards.cpp
+++ cards.cpp
@@ -1,16 +1,3 @@
-/*
- * Exercicio:
- * 
- * 1) Ler cartas da entrada padrão. OK
- * 2) Implementar alg. find_if() OK
- * 3) Implementar alg. filter()
- * 4) Aplicar nas situações:
- *      4A) Usar find_if para: Achar a primeira carta cujo valor da face seja uma figura (valete, dama, rei). OK
- *      4B) Usar find_if para: Achar a primeira carta que seja vermelha. OK
- *      4C) Usar filter para: Selecioar em um vetor apenas as cartas de "hearts"
- *      4C) Usar filter para: Selecioar apenas as cartas com figuras.
- */
-
 #include <iostream>
 
 // Cards defined as structs with suits and ranks
@@ -28,8 +15,7 @@ struct Card {
 };
 
 // ?
-std::ostream& operator<<( std::ostream& os, const Card & c )
-{
+std::ostream& operator<<( std::ostream& os, const Card & c ) {
     std::string ranks_to_str[]{"as", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "joker"};
     std::string suits_to_str[]{ "clubs", "diamonds", "hearts", "spades" };
     os << ranks_to_str[c.r];
@@ -38,8 +24,47 @@ std::ostream& operator<<( std::ostream& os, const Card & c )
     return os;
 }
 
-Card* find_if( Card *first, Card *last, bool (condition)(Card) )
-{	
+// Read cards from standard input and store them in vector
+void read_cards(Card *vector) {
+
+    short suit;
+    for (int i{0}; std::cin >> suit; ++i) {
+        switch (suit) {
+            case 0:
+                vector[i].s = Card::suit::clubs;
+                break;
+            case 1:
+                vector[i].s = Card::suit::diamonds;
+                break;
+            case 2:
+                vector[i].s = Card::suit::hearts;
+                break;
+            case 3:
+                vector[i].s = Card::suit::spades;
+                break;
+            default:
+                return ;
+        }
+        std::cin >> vector[i].r;
+    }
+
+}
+
+// TOFIX
+void print_cards(Card *first, Card *last) {
+
+    std::cout << "Cards are: " << "[ ";
+    for (Card *pointer{first}; pointer != last; ++pointer)
+        std::cout << *pointer << " ";
+    std::cout << " ]" << std::endl;
+
+}
+
+// TODO
+// Card* filter()
+
+// Return pointer to first card with matching condition
+Card* find_if( Card *first, Card *last, bool (condition)(Card) ) {	
 	for (Card *pointer{first}; pointer != last; ++pointer ) {
 		if (condition(*pointer))
 			return pointer;
@@ -67,34 +92,18 @@ int main(void)
 {
 
     Card hand[20];
-    int suit;
-    for (int i{0}; std::cin >> suit; ++i) {
-        switch (suit) {
-            case 0:
-                hand[i].s = Card::suit::clubs;
-                break;
-            case 1:
-                hand[i].s = Card::suit::diamonds;
-                break;
-            case 2:
-                hand[i].s = Card::suit::hearts;
-                break;
-            case 3:
-                hand[i].s = Card::suit::spades;
-                break;
-            default:
-                return 0;
-        }
-        std::cin >> hand[i].r;
-    }
-
     bool (*function)(Card);
 
-    function = is_red;
-    std::cout << "first red card: " << *(find_if(hand, std::end(hand), function)) << std::endl;
+    read_cards(hand);
+    // print_cards(hand, std::end(hand));
 
+    // Finding first red card
+    function = is_red;
+    std::cout << "First red card: " << *(find_if(hand, std::end(hand), function)) << std::endl;
+
+    // Finding first figured card
     function = is_figure;
-    std::cout << "first figured card: " << *(find_if(hand, std::end(hand), function)) << std::endl;
+    std::cout << "First figured card: " << *(find_if(hand, std::end(hand), function)) << std::endl;
 
     return 0;
 }
